fe6804f0ce7becb12423d3a3bf172a9dab212319
PAVAN KUMAR PALIVELA <pavankumarpalivela8@gmail.com>
2026-08-11 17:28:46 +0530

    feat: add transliteration route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Letters from "@/pages/Letters";
+import Transliteration from "@/pages/Transliteration";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/letters" component={Letters} />
+      <Route path="/transliteration" component={Transliteration} />
       <Route component={NotFound} />
     </Switch>
   );
